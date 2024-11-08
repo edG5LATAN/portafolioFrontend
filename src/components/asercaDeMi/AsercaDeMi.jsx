4c022992a76f46d847e756dfc9d75e0e6a35a631
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2024-11-08 15:45:55 -0600

    Update AsercaDeMi.jsx
    lista de mas aserca de mi

--- src/components/asercaDeMi/AsercaDeMi.jsx
+++ src/components/asercaDeMi/AsercaDeMi.jsx
@@ -11,7 +11,7 @@ function AsercaDeMi() {
             Soy tecnico en electronica y computacion egresado del INFOP y desarrollador Front-End con conocimientos en las
             areas de HTML, CSS, JS, Bootstrap, React, java, TypeScript, GitHub.<br/>
             Con conocimiento en Backend en Express de NodeJS, Spring Boot de Java.<br/>
-            Base da datos MYSQL ORACLE. Ingresando a Proyectos podras ver algunos de los trabajos realizados.
+            Base da datos MYSQL ORACLE con certificacion de ALURA LATAM, ORCALE y UDEMY. Da click a Proyectos ahi podras ver algunos de los trabajos realizados.
           </p>
         </div>
       </div>
