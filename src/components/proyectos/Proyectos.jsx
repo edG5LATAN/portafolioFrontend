643be782d70edcf4aff5d6798924d4e0e3a2cbc0
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2024-11-16 10:56:20 -0600

    se agrego curriculum nuevo

--- src/components/proyectos/Proyectos.jsx
+++ src/components/proyectos/Proyectos.jsx
@@ -14,12 +14,12 @@ function Proyectos() {
       catalogo: "https://www.youtube.com/watch?v=bmI1C-HTBXk",
     },
     {
-      img:"https://i.pinimg.com/originals/c5/dd/c2/c5ddc2e3b83e80fc0703061553819d59.png",
+      img: "https://i.pinimg.com/originals/c5/dd/c2/c5ddc2e3b83e80fc0703061553819d59.png",
       nombre: "Taller de Reparacion",
       detalles:
-        "Proyecto de uno de mis primeros trabajos ahora ya actualizado con la version V1.0.1 donde implementamos mas informacion."
-      demo:"https://tecnicare-taller-v1-0-1.vercel.app/",
-      catalago:"https://tecnicare-taller-v1-0-1.vercel.app/",
+        "Proyecto de uno de mis primeros trabajos ahora ya actualizado con la version V1.0.1 donde implementamos mas informacion.",
+      demo: "https://tecnicare-taller-v1-0-1.vercel.app/",
+      catalago: "https://tecnicare-taller-v1-0-1.vercel.app/",
     },
     {
       img: "./img/trabajos/aluraGeek.png",
@@ -88,177 +88,175 @@ function Proyectos() {
   ]);
   return (
     <>
-    <div className="container pt-3 pb-3 proyectos_contenedor">
-      <div
-        id="carouselExampleCaptions"
-        class="carousel slide bg"
-        data-bs-ride="carousel"
-      >
-        <div class="carousel-indicators ">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="0"
-            class="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="3"
-            aria-label="Slide 4"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="4"
-            aria-label="Slide 5"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="5"
-            aria-label="Slide 6"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="6"
-            aria-label="Slide 7"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="7"
-            aria-label="Slide 8"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="8"
-            aria-label="Slide 9"
-          ></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src={data[0].img} class="d-block proyectos_img" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>{data[0].nombre}</h5>
-              <p>{data[0].detalles}</p>
-              <Botones demo={data[0].demo} catalogo={data[0].catalogo} />
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src={data[1].img} class="d-block proyectos_img" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>{data[1].nombre}</h5>
-              <p>{data[1].detalles}</p>
-              <Botones demo={data[1].demo} catalogo={data[1].catalogo} />
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src={data[2].img} class="d-block proyectos_img" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>{data[2].nombre}</h5>
-              <p>{data[2].detalles}</p>
-              <Botones demo={data[2].demo} catalogo={data[2].catalogo} />
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src={data[3].img} class="d-block proyectos_img" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>{data[3].nombre}</h5>
-              <p>{data[3].detalles}</p>
-              <Botones demo={data[3].demo} catalogo={data[3].catalogo} />
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src={data[4].img} class="d-block proyectos_img" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>{data[4].nombre}</h5>
-              <p>{data[4].detalles}</p>
-              <Botones demo={data[4].demo} catalogo={data[4].catalogo} />
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src={data[5].img} class="d-block proyectos_img" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>{data[5].nombre}</h5>
-              <p>{data[5].detalles}</p>
-              <Botones demo={data[5].demo} catalogo={data[5].catalogo} />
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src={data[6].img} class="d-block proyectos_img" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>{data[6].nombre}</h5>
-              <p>{data[6].detalles}</p>
-              <Botones demo={data[6].demo} catalogo={data[6].catalogo} />
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src={data[7].img} class="d-block proyectos_img" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>{data[7].nombre}</h5>
-              <p>{data[7].detalles}</p>
-              <Botones demo={data[7].demo} catalogo={data[7].catalogo} />
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img src={data[8].img} class="d-block proyectos_img" alt="..." />
-            <div class="carousel-caption d-none d-md-block">
-              <h5>{data[8].nombre}</h5>
-              <p>{data[8].detalles}</p>
-              <Botones demo={data[8].demo} catalogo={data[8].catalogo} />
-            </div>
-          </div>
-        </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
+      <div className="container pt-3 pb-3 proyectos_contenedor">
+        <div
+          id="carouselExampleCaptions"
+          class="carousel slide bg"
+          data-bs-ride="carousel"
         >
-          <span
-            class="caroucel_contrl carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
-        >
-          <span
-            class="carousel-control-next-icon caroucel_contrl"
-            aria-hidden="true"
-          ></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+          <div class="carousel-indicators ">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="0"
+              class="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="3"
+              aria-label="Slide 4"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="4"
+              aria-label="Slide 5"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="5"
+              aria-label="Slide 6"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="6"
+              aria-label="Slide 7"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="7"
+              aria-label="Slide 8"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="8"
+              aria-label="Slide 9"
+            ></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src={data[0].img} class="d-block proyectos_img" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>{data[0].nombre}</h5>
+                <p>{data[0].detalles}</p>
+                <Botones demo={data[0].demo} catalogo={data[0].catalogo} />
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src={data[1].img} class="d-block proyectos_img" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>{data[1].nombre}</h5>
+                <p>{data[1].detalles}</p>
+                <Botones demo={data[1].demo} catalogo={data[1].catalogo} />
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src={data[2].img} class="d-block proyectos_img" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>{data[2].nombre}</h5>
+                <p>{data[2].detalles}</p>
+                <Botones demo={data[2].demo} catalogo={data[2].catalogo} />
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src={data[3].img} class="d-block proyectos_img" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>{data[3].nombre}</h5>
+                <p>{data[3].detalles}</p>
+                <Botones demo={data[3].demo} catalogo={data[3].catalogo} />
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src={data[4].img} class="d-block proyectos_img" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>{data[4].nombre}</h5>
+                <p>{data[4].detalles}</p>
+                <Botones demo={data[4].demo} catalogo={data[4].catalogo} />
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src={data[5].img} class="d-block proyectos_img" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>{data[5].nombre}</h5>
+                <p>{data[5].detalles}</p>
+                <Botones demo={data[5].demo} catalogo={data[5].catalogo} />
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src={data[6].img} class="d-block proyectos_img" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>{data[6].nombre}</h5>
+                <p>{data[6].detalles}</p>
+                <Botones demo={data[6].demo} catalogo={data[6].catalogo} />
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src={data[7].img} class="d-block proyectos_img" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>{data[7].nombre}</h5>
+                <p>{data[7].detalles}</p>
+                <Botones demo={data[7].demo} catalogo={data[7].catalogo} />
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src={data[8].img} class="d-block proyectos_img" alt="..." />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>{data[8].nombre}</h5>
+                <p>{data[8].detalles}</p>
+                <Botones demo={data[8].demo} catalogo={data[8].catalogo} />
+              </div>
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev"
+          >
+            <span
+              class="caroucel_contrl carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next"
+          >
+            <span
+              class="carousel-control-next-icon caroucel_contrl"
+              aria-hidden="true"
+            ></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
-    </div>
-    <div className="proyectos_escondidos container mb-2 ">
-      {
-        data.map(res=>{
-          return<ProyectoMin info={res} />
-        })
-        }
-    </div>
+      <div className="proyectos_escondidos container mb-2 ">
+        {data.map((res) => {
+          return <ProyectoMin info={res} />;
+        })}
+      </div>
     </>
   );
 }
